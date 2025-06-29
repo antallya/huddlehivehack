ada4ec601d6c28664575fbaa951684e6b554e420
antallya <antalymp@gmail.com>
2025-06-29 11:43:26 +0100

    Fix navigation links for local development and production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
 
   app: {
-    baseURL: '/huddlehivehack/',
+    baseURL: process.env.NODE_ENV === 'production' ? '/huddlehivehack/' : '/',
     head: {
       title: 'vennU - Third Space Social Mood Marketplace',
       meta: [
